055a9103ca561f0fd2e65f801a6b9beafabaf9b5
nandanpytech <bilaginandan333@gmail.com>
2023-04-17 20:27:13 +0530

    push

--- src/FetchData/RestaurantData.js
+++ src/FetchData/RestaurantData.js
@@ -5,7 +5,7 @@ export const allRestaurant=async(coordinate,sorttype)=>{
     const Rescards=await Data.json()
     return Rescards
     }
-    const Data=await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.2958104&lng=76.6393805&sortBy=${sorttype}&page_type=DESKTOP_WEB_LISTING`)
+    const Data=await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9762&lng=77.6033&sortBy=${sorttype}&page_type=DESKTOP_WEB_LISTING`)
     const Rescards=await Data.json()
     return Rescards
     
